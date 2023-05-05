eae9ae45465789a215e2dcc6251d9f4b162fa5d4
Ryan Elfman <ryan.elfman@planet.com>
2023-05-05 09:48:30 +0200

    add a url_template to the config

---
+++ src/stactools/browse/config_tile.js
@@ -0,0 +1,4 @@
+buildTileUrlTemplate: ({href}) => 'https://tiles.planet-labs.com/scenes/{href}/gmap/{z}/{x}/{y}.png?mask_types=sensor&sensor_mask=false&auth_token=burritos4lyfe'
+export { buildTileUrlTemplate };
+
+
